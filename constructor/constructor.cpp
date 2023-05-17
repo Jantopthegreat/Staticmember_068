f4e33e5664c106c7875118b74de3bd2c8a21e059
Jantopthegreat <fauzangriya@gmail.com>
2023-05-17 18:03:09 +0700

    menambahkan

--- constructor/constructor.cpp
+++ constructor/constructor.cpp
@@ -12,3 +12,24 @@ public:
 	mahasiswa(string);
 	mahasiswa(int iNim, string iNama);
 	void cetak();
+};
+
+mahasiswa::mahasiswa() {
+	nim = 0;
+	nama = "";
+}
+
+mahasiswa::mahasiswa(int iNim) {
+	nim = iNim;
+}
+
+mahasiswa::mahasiswa(string iNama) {
+	nama = iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama) {
+	nim = iNim;
+	nama = iNama;
+}
+
+}
