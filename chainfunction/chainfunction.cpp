de6aabd45a081adc44a89ee0e44fa6479563c144
Jantopthegreat <fauzangriya@gmail.com>
2023-05-17 17:52:16 +0700

    membuat setJudul dan getJudul

--- chainfunction/chainfunction.cpp
+++ chainfunction/chainfunction.cpp
@@ -2,4 +2,18 @@
 using namespace std;
 class buku
 {
+	string judul;
+
+public:
+	buku setJudul(string judul)
+	{
+		this->judul = judul;
+		return *this; // chain fucntion
+	}
+	string getJudul()
+	{
+		return this->judul;
+	}
+;
+}
 	
