63b45b6440dbded0eea3188c65330842d07ceadb
Jantopthegreat <fauzangriya@gmail.com>
2023-05-17 11:18:27 +0700

    membuat member untuk class mahasiswa

--- Staticmember_068/Staticmember_068.cpp
+++ Staticmember_068/Staticmember_068.cpp
@@ -4,4 +4,15 @@ using namespace std;
 
 class mahasiswa {
 public:
-	
+	static int nim;
+	int id;
+	string nama;
+
+
+	void setId();
+	void printAll();
+	mahasiswa(string pnama) :nama(pnama) { setId(); }
+
+};
+
+
